fc5982829fb2ec178a23f8f7b5c465982d3383dc
Colette <colette.rs.shaw@gmail.com>
2021-07-06 18:20:53 +0100

    Remove comments

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ function showMessage(message) {
   document.getElementById(id).style.color = "#F0C3D6";
 }
 
-// This doesn't work because I'm calling the functions straight away...
-// If someone could tell me wtf I was *supposed* to do considering I want to pass in parameters, that would be great.
-
-// Because this way of dealing with it is gross and stupid and can't be what was intended.
-
-// Okay never mind, apparently I was supposed to use anonymous functions.
-
 // window.onload = function intro() {
 //   // alert("test");
 //   setInterval(showMessage("intro"), 20000);
